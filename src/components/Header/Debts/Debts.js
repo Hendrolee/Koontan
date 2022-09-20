3c827b03282a5176bbb3aa0c204b3afb2792f5a7
Hendro Lee <hendrolee39@gmail.com>
2022-09-20 19:24:23 +1000

    Add new components to Debts folder

--- src/components/Header/Debts/Debts.js
+++ src/components/Header/Debts/Debts.js
@@ -1,13 +1,31 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import DebtsList from "./DebtsList";
 
 const Debts = (props) => {
   const [debtsSummary, setDebtsSummary] = useState([]);
 
-  const arrayIsEmpty = debtsSummary.length === 0;
+  const expenses = useSelector((state) => state.expenses.expense);
+  const arrayIsEmpty = expenses.length === 0;
+
+  let filteredValue = [];
+  console.log(expenses);
+
+  // filter the sharedWith user
+  for (let i = 0; i < expenses.length; i++) {
+    let searchUser = expenses.filter(
+      (expense) => expense.sharedWith[i].value === "Patrick"
+    );
+
+    if (searchUser.length !== 0) {
+      filteredValue.push(searchUser);
+    }
+  }
 
   return (
     <div>
       {arrayIsEmpty && <p>You are clean of debts!</p>}
+      <DebtsList value={filteredValue} />
       <button onClick={props.onCancel}>Close</button>
     </div>
   );
