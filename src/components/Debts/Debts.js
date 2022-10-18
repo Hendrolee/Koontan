e9eb3c428482b1715ade77a505bd7ebc5b82b77a
Hendro Lee <hendrolee39@gmail.com>
2022-10-18 21:01:36 +1100

    Refactor: codes to filter user's debt with function call.

--- src/components/Debts/Debts.js
+++ src/components/Debts/Debts.js
@@ -1,29 +1,20 @@
 import { useSelector } from "react-redux";
 import DebtsList from "./DebtsList";
 import Button from "../UI/Button/Button";
+import { createDebt, findUserExpenses } from "../../utils/debtFilter";
 
 const Debts = (props) => {
   const expenses = useSelector((state) => state.expenses.expenseItems);
   const arrayIsEmpty = expenses.length === 0;
 
   // filter the sharedWith user (Hardcoded)
-  const filteredValue = expenses
-    .filter((item) =>
-      item.sharedWith.find((member) => member.value === "Patrick")
-    )
-    .map((filteredExpense) => {
-      let numberOfUsers = filteredExpense.sharedWith.length;
-      let newObject = {
-        ...filteredExpense,
-        payable: (filteredExpense.amount / numberOfUsers).toFixed(2),
-      };
-      return newObject;
-    });
+  const userExpenses = findUserExpenses(expenses, "Patrick");
+  const debtObject = createDebt(userExpenses);
 
   return (
     <div>
       {arrayIsEmpty && <p>You are clean of debts!</p>}
-      <DebtsList value={filteredValue} />
+      <DebtsList value={debtObject} />
       <Button onClick={props.onCancel}>Close</Button>
     </div>
   );
